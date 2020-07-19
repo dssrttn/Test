e6fd784ab2e35a68a16a6ca1b01b87371e3b6b04
Mein Vorname <meinevornamae@lsdcsfsfl.de>
2020-07-19 13:22:50 +0200

    wflow_publish("code/")

--- code/Boxplot_DIL_mean.R
+++ code/Boxplot_DIL_mean.R
@@ -9,23 +9,23 @@ DIL_mean_boxplot
 DIL_Boost_mean_boxplot <- DIL_mean_boxplot %>% add_column(Mittelwert_Boost = Boost_mean_boxplot$Mittelwert)
 DIL_Boost_mean_boxplot
 
-DIL_mean_rel_table <- ddply(DIL_Boost_mean_boxplot, .(), transform, dilboost = (Mittelwert_DIL/ Mittelwert_Boost[1]))
-DIL_mean_rel_table
+DIL_Boost_mean_relation <- ddply(DIL_Boost_mean_boxplot, .(), transform, DILtoBoost_1 = (Mittelwert_DIL/ Mittelwert_Boost[1]))
+DIL_Boost_mean_relation
 
-DIL_mean_rel_table <- DIL_mean_rel_table %>% rename('Fraction' = '.id') #renmae column '.id' with 'Fraction'
-DIL_mean_rel_table[1:14, 1] = (1:14)
-DIL_mean_rel_table
+DIL_Boost_mean_relation <- DIL_Boost_mean_relation %>% rename('Fraction' = '.id') #renmae column '.id' with 'Fraction'
+DIL_Boost_mean_relation[1:14, 1] = (1:14)
+DIL_Boost_mean_relation
 
 
 
-max <-max(DIL_mean_rel_table$dilboost)
-min <-min(DIL_mean_rel_table$dilboost)
+max <-max(DIL_Boost_mean_relation$DILtoBoost_1)
+min <-min(DIL_Boost_mean_relation$DILtoBoost_1)
 
 par(bty="n") # keinen Rahmen
 par(family="serif") # Schriftart
-par(mar = c(5, 6.5, 2, 2)) # Abstände um den Plot. 2. Position y-Achse
+par(mar = c(0, 6.5, 2, 2)) # Abstände um den Plot. 2. Position y-Achse
 par(mgp=c(5,1,0)) # mpg: Abstand Label zu Achse
-boxplot(DIL_mean_rel_table$dilboost,
+boxplot(DIL_Boost_mean_relation$DILtoBoost_1,
         ylab= "DIL[n] to Boost[1]", #y-Achse-Beschriftung
         #yaxt="n", #suppresses y-axis. Brauch ich das?
         col="transparent", #Farbe des Boxplots
@@ -37,19 +37,20 @@ boxplot(DIL_mean_rel_table$dilboost,
         mgp=c(2.5,0.2,0) # mpg: Abstand Label zu Achse
         # , xpd = TRUE
         #labels = FALSE
+
 )
 
 
 
-DIL_mean_rel_table %>%
+DIL_Boost_mean_relation %>%
   kable(align = c("cc"), escape = FALSE, format ='html', caption = "DIL durch Boost[1") %>%
   kable_styling(bootstrap_options = c("striped", "hover", "condensed"),
                 full_width = TRUE)
 
-DIL_mean_rel_table <- cut(DIL_mean_rel_table$dilboost, c(-Inf, 0.95, 0.96, 0.97, 0.98, 0.99, 1.00, 1.01, 1.02, 1.03, 1.04, 1.05, Inf), labels=c('<95%', '95-96%', '96-97%', '97-98%', '98-99%', '99-100%', '100-101%', '101-102%', '102-103%', '103-104%', '104-105%', '>105%'))  # benutzt dplyr, 7 Intervalle
+DIL_Boost_mean_relation <- cut(DIL_Boost_mean_relation$DILtoBoost_1, c(-Inf, 0.95, 0.96, 0.97, 0.98, 0.99, 1.00, 1.01, 1.02, 1.03, 1.04, 1.05, Inf), labels=c('<95%', '95-96%', '96-97%', '97-98%', '98-99%', '99-100%', '100-101%', '101-102%', '102-103%', '103-104%', '104-105%', '>105%'))  # benutzt dplyr, 7 Intervalle
 
 
-DIL_mean_rel_table <- as.data.frame(DIL_mean_rel_table) %>%
-  group_by(DIL_mean_rel_table, .drop = FALSE) %>% #.drop = FALSE ansonsten Fehlermeldung, weil  empty categories are dropped by default. You can change this behavior by adding .drop = FALSE
+DIL_Boost_mean_relation <- as.data.frame(DIL_Boost_mean_relation) %>%
+  group_by(DIL_Boost_mean_relation, .drop = FALSE) %>% #.drop = FALSE ansonsten Fehlermeldung, weil  empty categories are dropped by default. You can change this behavior by adding .drop = FALSE
   dplyr::summarise(Numbers = n())  #%>% # %>% #=n())
 #mutate(Nummers = rowSums(.[-1]))
